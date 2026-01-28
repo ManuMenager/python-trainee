418d577fbdd91baa3a1c03f05a0f071187cc6706
ManuMenager <emanuelmenager2003@gmail.com>
2026-01-28 11:16:18 -0300

    Dia 1: Ejercicios

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 Dia 1: Fundamentos de Python
 - Repaso de tipos de datos, input, print, manejo de errores y condicionales.
 - Recordé que el uso de try/except es muy importante cuando es necesario castear un input.
+
+    Ejercicios.
+- Calculadora Simple: Utilice inputs que tomen los números y operador, condicionales para determinar que operación hacer y el try/except para manejar errores, ya sea una división por cero o un número u operador mal ingresado
+- Login Hardcodeado: Utilice variables constantes para recordar el usuario y contraseña correctos. un while para limitar la cantidad de intentos y condicionales para verificar los datos ingresados.
--- semana1/dia1_fundamentos.py
+++ semana1/dia1_fundamentos.py
@@ -1,5 +1,4 @@
 # Tipos, Manejo de errores y conversión.
-
 nombre = input("Ingrese su nombre: ")
 edad = input("Ingrese su edad: ")
 
@@ -34,3 +33,60 @@ elif 4 <= nota < 7:
     print("¡Aprobado!")
 else:
     print("¡Desaprobado!")
+
+# EJERCICIOS
+
+# Calculadora Simple
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+operador = input("Ingrese la operación (+, -, *, /): ")
+
+while True:
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+        if operador == '+':
+            print(num1 + num2)
+            break
+        elif operador == '-':
+            print(num1 - num2)
+            break
+        elif operador == '*':
+            print(num1 * num2)
+            break
+        elif operador == '/':
+            if num2 != 0:
+                print(num1 / num2)
+                break
+            else:
+                print("Error: División por cero no permitida.")
+                num2 = input("Ingrese el segundo número: ")
+        else:
+            print("Operación no válida. Por favor ingrese +, -, * o /.")
+            operador = input("Ingrese la operación (+, -, *, /): ")
+    except ValueError:
+        print("Por favor, ingrese números válidos.")
+        num1 = input("Ingrese el primer número: ")
+        num2 = input("Ingrese el segundo número: ")
+
+# Login hardcodeado
+CORRECT_USER = "admin"
+CORRECT_PASSWORD = "admin123"
+
+user = input("Ingrese su usuario: ")
+password = input("Ingrese su contraseña: ")
+
+cant_intentos = 0
+
+while cant_intentos < 3:
+    if user == CORRECT_USER and password == CORRECT_PASSWORD:
+        print("Acceso correcto.")
+        break
+    elif cant_intentos < 2:
+        cant_intentos += 1
+        print("Usuario o contraseña incorrectos. Intente de nuevo.")
+        user = input("Ingrese su usuario: ")
+        password = input("Ingrese su contraseña: ")
+    else:
+        print("Número máximo de intentos alcanzado. Acceso denegado.")
+        break
